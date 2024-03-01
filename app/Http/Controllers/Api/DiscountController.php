1905fde0ec56eba2db555fa726dc23d94c1c8bd3
Rudi <126459042+idurgit@users.noreply.github.com>
2024-03-01 16:58:49 +0700

    api post discount

--- app/Http/Controllers/Api/DiscountController.php
+++ app/Http/Controllers/Api/DiscountController.php
@@ -8,13 +8,33 @@ use Illuminate\Http\Request;
 class DiscountController extends Controller
 {
     //index
-    public function index() {
+    public function index()
+    {
         //get data discount
         $discounts = \App\Models\Discount::all();
 
         return response()->json([
             'status' => 'success',
             'data' => $discounts
+        ], 200);
+    }
+
+    //store
+    public function store(Request $request)
+    {
+        //validate request
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'value' => 'required',
+        ]);
+
+        //create discount
+        $discount = \App\Models\Discount::create($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $discount
         ], 200);
     }
 }
--- database/migrations/2024_02_11_225310_create_products_table.php
+++ database/migrations/2024_02_11_225310_create_products_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('image')->nullable();
+            // $table->decimal('price', 10, 2);
             $table->decimal('price', 10, 2);
             $table->integer('stock');
             $table->boolean('status')->default(1);
